786899ba205a608e4b949f9254a29629a7f8b54a
Bruno Carvalho Filadelfo <Bruno.Filadelfo@Outlook.Com>
2021-10-13 22:19:29 -0300

    Add files via upload

---
+++ 6 condicionais (2-2)/script.js
@@ -0,0 +1,27 @@
+// string
+
+var hora = 15;
+
+if (hora < 12 ) {
+  console.log ("Bom dia"); 
+}
+
+if (hora <18 ) {
+  console.log("Boa tarde");
+}
+
+if (hora <23 ) {
+  console.log("Boa noite");
+}
+
+// string
+
+var hora = 15;
+
+if (hora <12 ) {
+  console.log("Bom dia");
+} else if ( hora < 18 ) {
+  console.log("Boa tarde") 
+} else if (hora <= 23 ) {
+  console.log("Boa noite");
+}
